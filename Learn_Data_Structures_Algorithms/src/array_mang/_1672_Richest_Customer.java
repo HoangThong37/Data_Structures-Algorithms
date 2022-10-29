2f78ee16102ffd02368dcfd6ed7cefb3df96cb8e
HoangThong37 <thonghoang115@gmail.com>
2022-10-30 00:23:52 +0700

    update

---
+++ Learn_Data_Structures_Algorithms/src/array_mang/_1672_Richest_Customer.java
@@ -0,0 +1,37 @@
+package array_mang;
+
+public class _1672_Richest_Customer {
+    public static int maximumWealth(int[][] a) {
+        int soKhachHang = a.length;
+        int soNganHang = a[0].length;
+        int[] tongTaiSan = new int[soKhachHang];
+
+        // tính tổng số tiền của mỗi khách hàng
+        for (int i = 0; i < soKhachHang; i++) {
+            int tinhTongTienKH = 0;
+            for (int j = 0; j < soNganHang; j++) {
+                tinhTongTienKH += a[i][j];
+            }
+            tongTaiSan[i] = tinhTongTienKH;
+        }
+        int max = 0;
+        for (int i = 0; i < soKhachHang; i++) {
+            if (tongTaiSan[i] > max) {
+                max = tongTaiSan[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {{1,2,3}, {4,5,6}};
+        System.out.println(maximumWealth(a));
+    }
+}
+
+//Input: accounts = [[1,2,3],[3,2,1]]
+//        Output: 6
+//        Explanation:
+//        1st customer has wealth = 1 + 2 + 3 = 6
+//        2nd customer has wealth = 3 + 2 + 1 = 6
+//        Both customers are considered the richest with a wealth of 6 each, so return 6.
---
+++ Learn_Data_Structures_Algorithms/src/array_mang/_27_Remove_Element.java
@@ -0,0 +1,40 @@
+package array_mang;
+
+public class _27_Remove_Element {
+    //    public static int removeElement(int[] nums, int val) {
+//        int k = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] != val) {
+//                nums[k] = nums[i];
+//                k++;
+//            }
+//        }
+//        return k;
+//    }
+    public static int removeElement(int[] nums, int val) {
+        int num = nums.length;
+        for (int i = 0; i < num; ) {
+            if (nums[i] == val) {
+                // xóa
+                for (int j = i; j <= num -2; j++) {
+                    nums[j] = nums[j+1];
+                }
+                num--;
+            } else {
+                i++;
+            }
+        }
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3, 3, 5};
+        System.out.println(removeElement(nums, 2));
+    }
+}
+
+//Input:  nums = [3,2,2,3], val = 3
+//        Output: 2, nums = [2,2,_,_]
+//        Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+//        It does not matter what you leave beyond the returned k (hence they are underscores).
